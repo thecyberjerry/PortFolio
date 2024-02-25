b927ff6346b939db77dd2303d33ee6a69d493c32
thecyberjerry <himanshusharma2719@gmail.com>
2024-02-25 12:31:18 +0530

    tenth commit API

--- pages/api/getblog.js
+++ pages/api/getblog.js
@@ -1,15 +1,14 @@
 import * as fs from 'fs';
+import path from 'path';
 export default function handler(req, res) {
-
-    fs.readFile(`blogs/${req.query.blog}.json`,'utf-8',(err,data)=>
-    {
-        if(data)
-        {
-          res.status(200).json(JSON.parse(data))
-        }
-        else
-        {
-          res.status(404).json({error:"Blog Not Found"}) 
-        }
-    })
-  }
+  const filePath = path.join(process.cwd(), 'blogs',`${req.query.blog}.json`);
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (data) {
+      console.log(data)
+      res.status(200).json(JSON.parse(data))
+    }
+    else {
+      res.status(404).json({ error: "Blog Not Found" })
+    }
+  })
+}
