59ce9ea8ea683f09f1db75ad536c7bcde0d706c5
thecyberjerry <himanshusharma2719@gmail.com>
2024-02-25 11:36:45 +0530

    fourth commit bug fix

--- pages/allblogs/[blog].js
+++ pages/allblogs/[blog].js
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Head from 'next/head';
-import { Montserrat } from 'next/font/google'
-const montserrat = Montserrat({ weight: ['300'], subsets: ['latin'] })
+import { Montserrat } from 'next/font/google';
+const montserrat = Montserrat({ weight: ['300'], subsets: ['latin'] });
 
 export default function Blog({ data }) {
-  const [blog, setblog] = useState(data)
+  const [blog, setBlog] = useState(data); // Corrected useState function name
+
   function createMarkup(c) {
     return { __html: c };
   }
 
   return (
     <>
-
       <Navbar />
       <div className={`${montserrat.className}  flex text-left`}>
         <div>
@@ -26,7 +26,6 @@ export default function Blog({ data }) {
                 {blog.title}
               </div>
               <div className='px-10 py-10  md:px-64' dangerouslySetInnerHTML={createMarkup(blog.content)}>
-
                 {/* {blog.content} */}
               </div>
               <div className='flex text-center justify-end px-20 md:px-64'>
@@ -38,19 +37,20 @@ export default function Blog({ data }) {
       </div>
       <Footer />
     </>
-  )
+  );
 }
+
 export async function getServerSideProps(context) {
   const { blog } = context.query;
-  const res = await fetch(`http://localhost:3000/api/getblog?blog=${blog}`)
-  const data = await res.json()
+  const res = await fetch(`http://localhost:3000/api/getblog?blog=${blog}`);
+  const data = await res.json();
   if (res.status === 404) {
     return {
       redirect: {
         permanent: false,
         destination: '/blogs'
       }
-    }
+    };
   }
-  return { props: { data } }
+  return { props: { data } };
 }
--- pages/blogs.js
+++ pages/blogs.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import { useEffect, useState } from 'react'
-import Head from 'next/head'
-import { Montserrat } from 'next/font/google'
-import Link from 'next/link'
-const montserrat = Montserrat({ weight: ['300'], subsets: ['latin'] })
+import React, { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Head from 'next/head';
+import { Montserrat } from 'next/font/google';
+import Link from 'next/link';
 
-export default function blogs({ repo }) {
+const montserrat = Montserrat({ weight: ['300'], subsets: ['latin'] });
 
+export default function Blogs({ repo }) {
   useEffect(() => {
     import("@lottiefiles/lottie-player");
-  });
+  }, []);
 
-  const [blogdata, setblogdata] = useState(repo)
+  const [blogdata, setBlogData] = useState(repo);
+
   return (
     <>
       <div className={`${montserrat.className}`}>
         <Head>
           <title>Blogs</title>
-
         </Head>
         <Navbar />
         <div className='text-center text-3xl md:text-6xl  flex flex-col items-center justify-center'>
@@ -34,39 +33,35 @@ export default function blogs({ repo }) {
           <p className='border-4 border-transparent border-b-yellow-300'>Some of my blogs</p>
         </div>
         <hr className='my-10' />
-        {blogdata && blogdata.map((item) => {
-          return (<>
-            <section className="text-gray-600 my-28 body-font" key={item.slug}>
-              <div className="container px-5 mx-auto">
-                <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-                  <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-yellow-100 text-yellow-500 flex-shrink-0">
-                    <i className="text-[4rem] fa-solid fa-blog"></i>
-                  </div>
-                  <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-                    <h2 className="text-gray-900 text-3xl  title-font font-medium mb-2">{item.title}</h2>
-                    <p className="leading-relaxed text-base">{item.desc.slice(0, 150)}...</p>
-                    <p className='text-sm  py-5'>By {item.author + " on" + item.date}</p>
-                    <Link href={`/allblogs/${item.slug}`} className="mt-3 text-yellow-500 inline-flex items-center cursor-pointer">Read Blog
-                      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                      </svg>
-                    </Link>
-                  </div>
+        {blogdata && blogdata.map((item) => (
+          <section className="text-gray-600 my-28 body-font" key={item.slug}>
+            <div className="container px-5 mx-auto">
+              <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+                <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-yellow-100 text-yellow-500 flex-shrink-0">
+                  <i className="text-[4rem] fa-solid fa-blog"></i>
+                </div>
+                <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+                  <h2 className="text-gray-900 text-3xl  title-font font-medium mb-2">{item.title}</h2>
+                  <p className="leading-relaxed text-base">{item.desc.slice(0, 150)}...</p>
+                  <p className='text-sm  py-5'>By {item.author + " on" + item.date}</p>
+                  <Link href={`/allblogs/${item.slug}`} className="mt-3 text-yellow-500 inline-flex items-center cursor-pointer">Read Blog
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </Link>
                 </div>
               </div>
-            </section>
-          </>
-          )
-        })}
-
+            </div>
+          </section>
+        ))}
         <Footer />
       </div>
     </>
-  )
+  );
 }
 
 export async function getServerSideProps() {
-  const res = await fetch('http://localhost:3000/api/fetchblogs')
-  const repo = await res.json()
-  return { props: { repo } }
+  const res = await fetch('http://localhost:3000/api/fetchblogs');
+  const repo = await res.json();
+  return { props: { repo } };
 }
