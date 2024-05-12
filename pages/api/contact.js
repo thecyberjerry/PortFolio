e50be659671faafeca74f6b85608a3f68db3e539
thecyberjerry <himanshusharma2719@gmail.com>
2024-05-12 21:05:12 +0530

    api update

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,12 +1,12 @@
 import * as fs from 'fs';
 export default async function handler(req, res) {
-    if (req.method == 'POST') {
-        let file = await fs.promises.readdir(`ContactsData/`)
-        await fs.promises.writeFile(`ContactsData/User${file.length + 1}.json`, req.body)
-        res.status(200).json({ status: "success" })
-    }
-    else {
+    // if (req.method == 'POST') {
+    //     let file = await fs.promises.readdir(`ContactsData/`)
+    //     await fs.promises.writeFile(`ContactsData/User${file.length + 1}.json`, req.body)
+    //     res.status(200).json({ status: "success" })
+    // }
+    // else {
 
-        res.status(200).json({ status: "Fail" });
-    }
+        res.status(200).json({ status: "This Endpoint is not supported please goto link provided here", url:"https://himanshu-folio.vercel.app/contact" });
+    // }
 }
