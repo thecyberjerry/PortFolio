c4dea26a71d229acf3fb25548ba567526012cb69
thecyberjerry <himanshusharma2719@gmail.com>
2024-12-15 16:02:38 +0530

    rightback bug fix 1.3

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import Wewillbeback from "./wewillbeback";
+
 export default function App({ Component, pageProps }) {
   // return <Component {...pageProps} />;
   return <Wewillbeback />;
