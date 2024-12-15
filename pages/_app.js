e46808380f6c7de3fb232e47c07212f1cf4e2986
thecyberjerry <himanshusharma2719@gmail.com>
2024-12-15 16:45:13 +0530

    files update 1.1

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,5 @@ import "@/styles/globals.css";
 import Rightback from "./components/Rightback";
 
 export default function App({ Component, pageProps }) {
-  // return <Component {...pageProps} />;
   return <Rightback />
 }
