35ace4b54504ccb32a587e25958e6f4c6747fa2b
thecyberjerry <himanshusharma2719@gmail.com>
2025-01-26 12:22:42 +0530

    26 Jan changes

--- src/app/api/save-contact/route.ts
+++ src/app/api/save-contact/route.ts
@@ -1,11 +1,39 @@
-export const revalidate = 60;
 import prisma from "../../../../lib/prisma";
+import nodemailer from "nodemailer";
 export async function POST(request: Request) {
   try {
-    const saveContactToDatabase = await request.json();
-    await prisma.contact.create({
-      data: saveContactToDatabase,
+    const getContactData = await request.json();
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAILER_HOST,
+      port: 587,
+      tls: {
+        ciphers: "SSLv3",
+        rejectUnauthorized: false,
+      },
+
+      auth: {
+        user: process.env.MAILER_USER,
+        pass: process.env.MAILER_PASS,
+      },
     });
+    const saveContactToDB = await prisma.contact.create({
+      data: getContactData,
+    });
+    if (saveContactToDB) {
+      try {
+        await transporter.sendMail({
+          from: process.env.MAILER_FROM,
+          to: process.env.MAILER_TO,
+          replyTo: process.env.MAILER_REPLY_TO,
+          subject: `Someone contacted from your portfolio contact form`,
+          html: `
+          <p>Name: ${getContactData?.name} </p>
+          <p>Email:  ${getContactData?.email}</p>
+          <p>Message: ${getContactData?.message} </p>
+          `,
+        });
+      } catch (error) {}
+    }
     return Response.json({ status: true, code: 201 });
   } catch (error) {
     return Response.json({ status: false, message: error });
--- src/app/form.config.js
+++ src/app/form.config.js
@@ -18,7 +18,7 @@ export const config = {
 };
 export const formConfigValues = {
   name: { minLength: 10, maxLength: 20 },
-  email: { emailMaxLength: 20 },
+  email: { emailMaxLength: 50 },
   message: { minMessageLength: 100, maxMessageLength: 1000 },
   budgetAmount: { minBudget: 1000, maxBudget: 999999 },
 };
